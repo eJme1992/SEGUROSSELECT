084c83a4d31d5b4c60dd2223322fc082471e0c3d
eJme1992 <edwin.jme@hotmail.com>
2019-12-19 16:01:13 -0300

    Inicio 2.0

--- database/migrations/2019_12_19_171317_create_estados_table.php
+++ database/migrations/2019_12_19_171317_create_estados_table.php
@@ -4,28 +4,29 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateEstadosTable extends Migration
-{
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('estados', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->timestamps();
-        });
-    }
+class CreateEstadosTable extends Migration {
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up() {
+		Schema::create('estados', function (Blueprint $table) {
+			$table->bigIncrements('id');
+			$table->unsignedBigInteger('auto_id')->unsigned();
+			$table->string('año');
+			$table->string('estado');
+			$table->timestamps();
+			$table->foreign('auto_id')->references('id')->on('autos');
+		});
+	}
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('estados');
-    }
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down() {
+		Schema::dropIfExists('estados');
+	}
 }
--- database/migrations/2019_12_19_174355_create_requests_table.php
+++ database/migrations/2019_12_19_174355_create_requests_table.php
@@ -4,28 +4,30 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateRequestsTable extends Migration
-{
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('requests', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->timestamps();
-        });
-    }
+class CreateRequestsTable extends Migration {
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up() {
+		Schema::create('requests', function (Blueprint $table) {
+			$table->bigIncrements('id');
+			$table->unsignedBigInteger('city_id')->unsigned();
+			$table->string('año');
+			$table->string('estado');
+			$table->timestamps();
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('requests');
-    }
+			$table->foreign('auto_id')->references('id')->on('autos');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down() {
+		Schema::dropIfExists('requests');
+	}
 }
