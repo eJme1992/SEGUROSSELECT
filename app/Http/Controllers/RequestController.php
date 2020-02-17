6caf84ef8f8f872eee88bedf9087f813efbb62c5
eJme1992 <edwin.jme@hotmail.com>
2020-02-17 14:31:30 -0300

    smartix

--- app/Http/Controllers/RequestController.php
+++ app/Http/Controllers/RequestController.php
@@ -10,12 +10,38 @@ class RequestController extends Controller {
 
     public function Conexion()
     {
-      $Smartix = new Smartix();
-
-
-
-     
-      $Smartix->GetConfiguration();
+      $Smartix = new Smartix();  
+      return $Smartix->GetConfiguration();
     }
+
+    public function Cotizar(Request $request)
+    { 
+         
+        
+          $AnioModelo        =  $request->input('estado');
+          $CodigoProvincia   =  $request->input('provincia'); 
+          $DateTime          =  $request->input('nacimiento');
+          $IdVehiculoSmartix =  $request->input('auto'); 
+          $ValorGnc          =  $request->input('valor-gns');
+          $CodigoPostal      =  $request->input('postal');
+          
+          //$CeroKm            =  $request->input('nombrec');
+          //$CodigoEstadoCivil =  $request->input('nombrec');
+          //$CodigoTipoIva     =  $request->input('nombrec');
+          //$CodigoTipoUso     =  $request->input('nombrec');
+         
+
+          
+  
+
+    
+      $Smartix = new Smartix();  
+      $respuesta = $Smartix->IniciarCotizacion($AnioModelo, $CeroKm, $CodigoProvincia,$CodigoTipoIva,$CodigoTipoUso,$DateTime,$IdVehiculoSmartix,$ValorGnc,$CodigoEstadoCivil,$CodigoPostal);
+    }
+
+
+   
+
+
 }
 
